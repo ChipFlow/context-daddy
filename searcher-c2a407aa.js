f3e4de2f93181963aa6d73ebb3a83d0e587c3cc0
robtaylor <rob.taylor@chipflow.io>
2026-01-20 20:45:55 +0000

    Deploying to gh-pages from @ ChipFlow/context-daddy@fd3a2a548a5c4c642fc64289019410d5c1a4bfc1 🚀

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-01b5a084.js',
+                path_to_root + 'searchindex-989e942c.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
